afeba3076777d12b3f6b9d8bf40c25c006604d34
Leigh Allen <lallenation@gmail.com>
2024-04-29 10:21:02 +0000

    ad image filter and validation to the post method

--- posts/models.py
+++ posts/models.py
@@ -7,15 +7,32 @@ class Post(models.Model):
     Post model, relating to owner, the user isntance. 
     default image set to always reference image.url
     """
+    image_filter_choices = [
+        ('_1977', '1977'),
+        ('brannan', 'Brannan'),
+        ('earlybird', 'Earlybird'),
+        ('hudson', 'Hudson'),
+        ('inkwell', 'Inkwell'),
+        ('lofi', 'Lo-Fi'),
+        ('kelvin', 'Kelvin'),
+        ('normal', 'Normal'),
+        ('nashville', 'Nashville'),
+        ('rise', 'Rise'),
+        ('toaster', 'Toaster'),
+        ('valencia', 'Valencia'),
+        ('walden', 'Walden'),
+        ('xpro2', 'X-pro II')
+    ]
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
-    content = models = models.TextField(blank=True)
+    content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_profile_nwigev',
-        blank=True
+        upload_to='images/', default='../default_profile_nwigev'
     )
+    image_filter = models.CharField(
+        max_length=32, choices=image_filter_choices, default='normal')
 
     class Meta:
         ordering = ['-created_at']
