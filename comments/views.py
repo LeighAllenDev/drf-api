6b76bba233adeb377782171cd4724b27737bbd2d
Leigh Allen <lallenation@gmail.com>
2024-05-04 11:12:30 +0000

    add generics and comments section

--- comments/views.py
+++ comments/views.py
@@ -1,12 +1,12 @@
 from rest_framework import generics, permissions
-from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
+from drf_api.permissions import IsOwnerOrReadOnly
 
 
 class CommentList(generics.ListCreateAPIView):
-    serialize_class = CommentSerializer
-    permission_class = [permissions.IsOwnerOrReadOnly]
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
 
     def perform_create(self, serializer):
