380c50f1145907a5f3bd8fccdee476fed31aa3ad
Leigh Allen <lallenation@gmail.com>
2024-05-14 19:43:57 +0000

    change allowed hosts

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -62,7 +62,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['.gitpod.io', 'localhost', 'drf-api-lallenation.herokuapp.com']
+ALLOWED_HOSTS = ['.gitpod.io', 'localhost', '*.herokuapp.com']
 
 
 # Application definition
